c9b59d779003b27a472f1e72f3b6d68b81c32db0
bhavinisharma63-create <bhavinisharma63@gmail.com>
2025-08-24 18:42:24 +0530

    Add files via upload

---
+++ first assignment.py
@@ -0,0 +1,11 @@
+a=int(input('enter no.: '))
+b=int(input('enter no.: '))
+print('addition: ',a+b)
+print('multiplication: ',a*b)
+print('substraction: ',a-b)
+print('division: ',a/b)
+
+
+e =(input('enter your first name: '))
+f = (input('enter your last name: '))
+print('hello' + ' e'+' f'+'! Welcome to the python prgramming')
