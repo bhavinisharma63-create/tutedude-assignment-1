d4697d4ff10898836eabc30f41ec7565810c8d13
bhavini <bhavinisharma63@gmail.com>
2025-09-26 14:56:31 +0530

    Add files via upload

--- third.py
+++ third.py
@@ -2,19 +2,24 @@ Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-
 Enter "help" below or click "Help" above for more information.
 >>> #task 1
 ... 
-... a = int(input('enter the number: '))
-... print(a)
-... def factorial(n):
-...     if n == 0 or n == 1:
-...         return 1
-...     else:
-...         return n * factorial(n - 1)
-... result = factorial(a)
-... print("The factorial of", a, "is", result)
+..def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
 ... 
 ... # task 2
 ... 
-... b=int(input('enter the number: '))
-... from math import *
-... print("square: ",b**2)
-... print("logarithm: ",log10(b))
+... num = float(input("Enter a number: "))
+sqrt_val = math.sqrt(num)
+log_val = math.log(num) 
+sine_val = math.sin(num)      
+
+print(f"Square root of {num}: {sqrt_val}")
+print(f"Natural logarithm of {num}: {log_val}")
+print(f"Sine of {num} (in radians): {sine_val}")
